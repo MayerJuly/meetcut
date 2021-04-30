20e14875792f1c4803ef70a6f4b9165157c0d7da
MayerJuly <73027797+MayerJuly@users.noreply.github.com>
2021-04-30 15:27:27 +0300

    Animation Update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(function(){
+    $('.preloader__content').addClass('active')
 
     let burger = $('.header__burger')
     let nav = $('.header__nav')
@@ -10,5 +11,24 @@ $(function(){
         burger.toggleClass('active')
     })
 
+//    PRELOADER AUTO-TEXT
+    let preloaderText = $('#preloaderText')
+
+    function animateText() {
+        const string = preloaderText.text()
+        return preloaderText.each(function(){
+            const $preloaderText = $(this)
+            $preloaderText.html(string.replace(/./g, '<span class="new">$&</span>'))
+            $preloaderText.find('span.new').each(function(i, el){
+                setTimeout(function(){ $(el).addClass('div_opacity'); }, 70*i)
+            })
+        })
+    }
+
+    function showPreloaderText(){
+        preloaderText.css('opacity','1')
+        animateText()
+    }
+    showPreloaderText()
 
 });
